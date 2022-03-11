267433dbeb623b84905f6f7b4e6b21522bf3e457
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2022-03-11 11:33:52 -0300

    update project

--- Persistence/DevJobsContext.cs
+++ Persistence/DevJobsContext.cs
@@ -1,13 +1,29 @@
 using DevJobs.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevJobs.API.Persistence
 {
-    public class DevJobsContext
+    public class DevJobsContext : DbContext
     {
-        public DevJobsContext()
+        public DevJobsContext(DbContextOptions<DevJobsContext> options) : base(options)
         {
-            JobVacancies = new List<JobVacancy>();
+            
         }
-        public List<JobVacancy> JobVacancies {get; set;}
+        public DbSet<JobVacancy> JobVacancies {get; set;}
+        public DbSet<JobApplication> JobApplications {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.Entity<JobVacancy>(e => {
+                e.HasKey(jv => jv.Id);
+                //e.ToTable("tb_JobVacancies");
+                e.HasMany(jv => jv.Applications)
+                    .WithOne()
+                    .HasForeignKey(ja => ja.IdJobVacancy)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+            builder.Entity<JobApplication>(e => {
+                e.HasKey(ja => ja.Id);
+            });
+        }
     }
 }
--- obj/Debug/net6.0/DevJobs.API.AssemblyInfo.cs
+++ obj/Debug/net6.0/DevJobs.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("6b948b75-17c6-4416-837e-9d4f3e9bf1b3")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevJobs.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
